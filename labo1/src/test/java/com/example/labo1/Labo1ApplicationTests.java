02614d547364ee2ad919cdf6d9c20715f1aa5f50
Bradley Leneus <6243042@collegemv.qc.ca>
2025-08-28 09:58:31 -0400

    Création du projet  labo1

---
+++ labo1/src/test/java/com/example/labo1/Labo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
