e49112ea9b138527ac4a75f6750c03a0fd7b1a77
samuel <monkeygmaming222@gmail.com>
2025-09-11 20:12:05 -0400

    adapation en mariadb

---
+++ labo1/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into person (name, last_name, email, gender) values ("ssam", "theGoat", "hehe@gmail.com", "male");
+insert into person (name, last_name, email, gender) values ("sssdfsam", "theGoat", "hehe@gmail.com", "male");
+insert into person (name, last_name, email, gender) values ("sssdfsdfam", "theGoat", "hehe@gmail.com", "male");
+insert into person (name, last_name, email, gender) values ("ssfdsdfsfdsam", "theGoat", "hehe@gmail.com", "male");
+insert into person (name, last_name, email, gender) values ("sssssssssssam", "theGoat", "hehe@gmail.com", "male");
