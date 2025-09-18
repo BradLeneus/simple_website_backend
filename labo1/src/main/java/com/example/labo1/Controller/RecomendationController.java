40c28406c329ed4e04b62d6276020148348d6277
SamuelRobillard <samuelben.robillard@gmail.com>
2025-09-18 09:41:04 -0500

    ajout de la recommendation et tendance de serie

--- labo1/src/main/java/com/example/labo1/Controller/RecomendationController.java
+++ labo1/src/main/java/com/example/labo1/Controller/RecomendationController.java
@@ -1,13 +1,11 @@
 package com.example.labo1.Controller;
 
-import com.example.labo1.Model.History;
-import com.example.labo1.Model.Person;
 import com.example.labo1.Model.Series;
-import com.example.labo1.Repositories.RepositoryPerson;
-import com.example.labo1.Service.HistoryService;
+import com.example.labo1.Service.RecommendationSerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Dictionary;
 import java.util.List;
 
 @RestController
@@ -16,16 +14,21 @@ import java.util.List;
 public class RecomendationController {
 
     @Autowired
-    private HistoryService historyService;
+    private RecommendationSerieService recommendationSerieService;
 
 
-    @GetMapping("/{userId}")
-    public List<Series> findAllByUserId(@PathVariable int userId) {
-        return historyService.findSerieByUserId(userId);
+    @GetMapping("/gender")
+    public List<String> TrendingGender() {
+        return recommendationSerieService.getMostViewGender();
     }
-    @PostMapping("/{userId}/{serieId}")
-    public boolean addSerieToHistory(@PathVariable int userId, @PathVariable int serieId) {
-        return historyService.addASerieToHistory(userId, serieId);
+    @GetMapping("/tendence/{userId}")
+    public List<Series> TendenceSerieToUser(@PathVariable int userId) {
+        return recommendationSerieService.recommendNonWatchedSerie(userId, true);
     }
+    @GetMapping("/reco/{userId}")
+    public List<Series> reommendSerieToUser(@PathVariable int userId) {
+        return recommendationSerieService.recommendNonWatchedSerie(userId, false);
+    }
+
 
 }
